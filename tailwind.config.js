41b2acdcea5093d394fcdfe6550723b6b4c9827a
wensen2525 <109505910+wensen2525@users.noreply.github.com>
2023-05-30 15:54:50 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        'yellow-light': '#ffeeca',
+        'brown' : '#593c2c',
+        'yellow-orange' : '#ela23b',
+        'green-old' : '#496f24',
+        'green-yellow' : '#b5b90a',
+        'green-light' : '#a5d577',
+        'white' : '#ffffff',
+        'blue-light' : '#9cd1e1',
+      },
+    },
+  },
+  plugins: [],
+}
+
